5c702f439bf6915507c5189db0425400ed7cfe7f
Aniket Singh <aniketsinghofficial144@gmail.com>
2022-02-28 11:25:58 +0530

    day1

---
+++ ParkingSpacePicker.py
@@ -0,0 +1,9 @@
+import cv2
+import pickle
+
+img = cv2.imread('carParkImg.png')
+
+while True:
+    cv2.rectangle(img,(100,100),(200,150),(255,0,255),2)
+    cv2.imshow("image",img)
+    cv2.waitKey(0)
